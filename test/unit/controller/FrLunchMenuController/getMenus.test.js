74a5be197840cac0308cc98c64f84fdaf77354d1
kanami AKAMA <k-akama@cresco.co.jp>
2016-09-21 12:11:27 +0900

    update FrLunchMenuController unit test - modify 200 case and 400 case (use async)

--- test/unit/controller/FrLunchMenuController/getMenus.test.js
+++ test/unit/controller/FrLunchMenuController/getMenus.test.js
@@ -1,4 +1,5 @@
 var assert = require('chai').assert;
+var async = require('async');
 var sinon = require('sinon');
 var fakeWaterlineChainMethod  = require('waterline-fakes').fakeWaterlineChainMethod;
 var frLunchMenuController = require('../../../../api/controllers/FrLunchMenuController');
@@ -6,346 +7,143 @@ var frLunchMenuController = require('../../../../api/controllers/FrLunchMenuCont
 describe('FrLunchMenusController#getMenus', function () {
   var stub;
 
+  var expectMenus = [
+    {
+      'menu': 'ハンバーグ ポン酢かけ香味野菜のせ',
+      'price': 465,
+      'category': 'SPECIAL',
+      'orderDate': 20160905,
+      'nutritionalInfo': 'Energy 378kcal  Protain 20.2g  Lipid 20.9g Carbohydrate 38.8g  Salt 3.9g',
+      'recommend': 0,
+      'halalFood': 0,
+      'id': 1,
+      'createdAt': '2016-09-14T09:25:17.000Z',
+      'updatedAt': '2016-09-14T09:25:17.000Z'
+    }
+  ];
+
   afterEach(function() {
     stub.restore();
   });
 
-  it('should return 200 when orderDate and category are match', function (done) {
-
-    // should
-    var expect = [
-      {
-        'menu': 'ハンバーグ ポン酢かけ香味野菜のせ',
-        'price': 465,
-        'category': 'SPECIAL',
-        'orderDate': 20160905,
-        'nutritionalInfo': 'Energy 378kcal  Protain 20.2g  Lipid 20.9g Carbohydrate 38.8g  Salt 3.9g',
-        'recommend': 0,
-        'halalFood': 0,
-        'id': 1,
-        'createdAt': '2016-09-14T09:25:17.000Z',
-        'updatedAt': '2016-09-14T09:25:17.000Z'
-      }
-    ];
-    // given
-    var req = {
+  async.each([
+    {
+      desc: 'orderDate and category are match',
       query: {
         orderDate: '20160905',
         category:'SPECIAL'
       }
-    };
-    var res = {
-      json:function (status, message) {
-        // then
-        assert.equal(status, 200);
-        assert.deepEqual(message,expect);
-        done();
-      }
-    };
-    stub = sinon.stub(FrLunchMenu, 'find', fakeWaterlineChainMethod( {result: expect}));
-
-    // when
-    frLunchMenuController.getMenus(req, res);
-  });
-
-  it('should return 200 when orderDate is match', function (done) {
-
-    // should
-    var expect = [
-      {
-        'menu': 'ハンバーグ ポン酢かけ香味野菜のせ',
-        'price': 465,
-        'category': 'SPECIAL',
-        'orderDate': 20160905,
-        'nutritionalInfo': 'Energy 378kcal  Protain 20.2g  Lipid 20.9g Carbohydrate 38.8g  Salt 3.9g',
-        'recommend': 0,
-        'halalFood': 0,
-        'id': 1,
-        'createdAt': '2016-09-14T09:25:17.000Z',
-        'updatedAt': '2016-09-14T09:25:17.000Z'
-      }
-    ];
-    // given
-    var req = {
+    },
+    {
+      desc: 'orderDate is match',
       query: {
         orderDate: '20160905'
       }
-    };
-    var res = {
-      json:function (status, message) {
-        // then
-        assert.equal(status, 200);
-        assert.deepEqual(message,expect);
-        done();
-      }
-    };
-    stub = sinon.stub(FrLunchMenu, 'find', fakeWaterlineChainMethod( {result: expect}));
-
-    // when
-    frLunchMenuController.getMenus(req, res);
-  });
-
-  it('should return 200 when category is match', function (done) {
-
-    // should
-    var expect = [
-      {
-        'menu': 'ハンバーグ ポン酢かけ香味野菜のせ',
-        'price': 465,
-        'category': 'SPECIAL',
-        'orderDate': 20160905,
-        'nutritionalInfo': 'Energy 378kcal  Protain 20.2g  Lipid 20.9g Carbohydrate 38.8g  Salt 3.9g',
-        'recommend': 0,
-        'halalFood': 0,
-        'id': 1,
-        'createdAt': '2016-09-14T09:25:17.000Z',
-        'updatedAt': '2016-09-14T09:25:17.000Z'
-      }
-    ];
-    // given
-    var req = {
+    },
+    {
+      desc: 'category is match',
       query: {
         category: 'SPECIAL'
       }
-    };
-    var res = {
-      json:function (status, message) {
-        // then
-        assert.equal(status, 200);
-        assert.deepEqual(message,expect);
-        done();
-      }
-    };
-    stub = sinon.stub(FrLunchMenu, 'find', fakeWaterlineChainMethod( {result: expect}));
-
-    // when
-    frLunchMenuController.getMenus(req, res);
-  });
-
-  it('should return 200 when orderDate is empty and category is match', function (done) {
-
-    // should
-    var expect = [
-      {
-        'menu': 'ハンバーグ ポン酢かけ香味野菜のせ',
-        'price': 465,
-        'category': 'SPECIAL',
-        'orderDate': 20160905,
-        'nutritionalInfo': 'Energy 378kcal  Protain 20.2g  Lipid 20.9g Carbohydrate 38.8g  Salt 3.9g',
-        'recommend': 0,
-        'halalFood': 0,
-        'id': 1,
-        'createdAt': '2016-09-14T09:25:17.000Z',
-        'updatedAt': '2016-09-14T09:25:17.000Z'
-      }
-    ];
-    // given
-    var req = {
+    },
+    {
+      desc: 'orderDate is empty and category is match',
       query: {
         orderDate: '',
         category: 'SPECIAL'
       }
-    };
-    var res = {
-      json:function (status, message) {
-        // then
-        assert.equal(status, 200);
-        assert.deepEqual(message,expect);
-        done();
-      }
-    };
-    stub = sinon.stub(FrLunchMenu, 'find', fakeWaterlineChainMethod( {result: expect}));
-
-    // when
-    frLunchMenuController.getMenus(req, res);
-  });
-
-  it('should return 200 when orderDate is match and category is empty', function (done) {
-
-    // should
-    var expect = [
-      {
-        'menu': 'ハンバーグ ポン酢かけ香味野菜のせ',
-        'price': 465,
-        'category': 'SPECIAL',
-        'orderDate': 20160905,
-        'nutritionalInfo': 'Energy 378kcal  Protain 20.2g  Lipid 20.9g Carbohydrate 38.8g  Salt 3.9g',
-        'recommend': 0,
-        'halalFood': 0,
-        'id': 1,
-        'createdAt': '2016-09-14T09:25:17.000Z',
-        'updatedAt': '2016-09-14T09:25:17.000Z'
-      }
-    ];
-    // given
-    var req = {
+    },
+    {
+      desc: 'orderDate is match and category is empty',
       query: {
         orderDate: '20160905',
         category: ''
       }
-    };
-    var res = {
-      json:function (status, message) {
-        // then
-        assert.equal(status, 200);
-        assert.deepEqual(message,expect);
-        done();
-      }
-    };
-    stub = sinon.stub(FrLunchMenu, 'find', fakeWaterlineChainMethod( {result: expect}));
-
-    // when
-    frLunchMenuController.getMenus(req, res);
-  });
-
-  it('should return 200 when orderDate and category are empty', function (done) {
-
-    // should
-    var expect = [
-      {
-        'menu': 'ハンバーグ ポン酢かけ香味野菜のせ',
-        'price': 465,
-        'category': 'SPECIAL',
-        'orderDate': 20160905,
-        'nutritionalInfo': 'Energy 378kcal  Protain 20.2g  Lipid 20.9g Carbohydrate 38.8g  Salt 3.9g',
-        'recommend': 0,
-        'halalFood': 0,
-        'id': 1,
-        'createdAt': '2016-09-14T09:25:17.000Z',
-        'updatedAt': '2016-09-14T09:25:17.000Z'
-      }
-    ];
-    // given
-    var req = {
+    },
+    {
+      desc: 'orderDate and category are empty',
       query: {
         orderDate: '',
         category: ''
       }
-    };
-    var res = {
-      json:function (status, message) {
-        // then
-        assert.equal(status, 200);
-        assert.deepEqual(message,expect);
-        done();
-      }
-    };
-    stub = sinon.stub(FrLunchMenu, 'find', fakeWaterlineChainMethod( {result: expect}));
-
-    // when
-    frLunchMenuController.getMenus(req, res);
-  });
-
-  it('should return 200 when orderDate and category don\'t exist', function (done) {
-
-    // should
-    var expect = [
-      {
-        'menu': 'ハンバーグ ポン酢かけ香味野菜のせ',
-        'price': 465,
-        'category': 'SPECIAL',
-        'orderDate': 20160905,
-        'nutritionalInfo': 'Energy 378kcal  Protain 20.2g  Lipid 20.9g Carbohydrate 38.8g  Salt 3.9g',
-        'recommend': 0,
-        'halalFood': 0,
-        'id': 1,
-        'createdAt': '2016-09-14T09:25:17.000Z',
-        'updatedAt': '2016-09-14T09:25:17.000Z'
-      }
-    ];
-    // given
-    var req = {
+    },
+    {
+      desc: 'orderDate and category don\'t exist',
       query: {}
-    };
-    var res = {
-      json:function (status, message) {
-        // then
-        assert.equal(status, 200);
-        assert.deepEqual(message,expect);
-        done();
-      }
-    };
-    stub = sinon.stub(FrLunchMenu, 'find', fakeWaterlineChainMethod( {result: expect}));
+    }
+  ], function (testcase, callback) {
+    it('should return 200 when ' + testcase.desc, function (done) {
+      // should
+      var expect = expectMenus;
 
-    // when
-    frLunchMenuController.getMenus(req, res);
+      // given
+      var req = {
+        query: testcase.query
+      };
+
+      var res = {
+        json:function (status, message) {
+          // then
+          assert.equal(status, 200);
+          assert.deepEqual(message, expect);
+          done();
+        }
+      };
+
+      stub = sinon.stub(FrLunchMenu, 'find', fakeWaterlineChainMethod( {result: expect}));
+
+      // when
+      frLunchMenuController.getMenus(req, res);
+    });
+
+    callback();
   });
 
-  it('should return 400 when orderDate isn\'t the 8 characters', function (done) {
-
-    // given
-    var req = {
+  async.each([
+    {
+      desc: 'orderDate isn\'t the 8 characters',
       query: {
         orderDate: '201609050'
       }
-    };
-    var res = {
-      send: function (status) {
-        // then
-        assert.equal(status, 400);
-        done();
-      }
-    };
-
-    // when
-    frLunchMenuController.getMenus(req, res);
-  });
-
-  it('should return 400 when orderDate isn\'t Numeric', function (done) {
-    // given
-    var req = {
+    },
+    {
+      desc: 'orderDate isn\'t Numeric',
       query: {
         orderDate: 'hogehoge'
       }
-    };
-    var res = {
-      send: function (status) {
-        // then
-        assert.equal(status, 400);
-        done();
-      }
-    };
-
-    // when
-    frLunchMenuController.getMenus(req, res);
-  });
-
-  it('should return 400 when category hasn\'t over 1 character below 50 characters', function (done) {
-    // given
-    var req = {
+    },
+    {
+      desc: 'category hasn\'t over 1 character below 50 characters',
       query: {
         category: '101010101020202020203030303030404040404050505050501'
       }
-    };
-    var res = {
-      send: function (status) {
-        // then
-        assert.equal(status, 400);
-        done();
-      }
-    };
-    // when
-    frLunchMenuController.getMenus(req, res);
-  });
-
-
-  it('should return 400 when category contains any symbol characters', function (done) {
-    // given
-    var req = {
+    },
+    {
+      desc: 'category contains any symbol characters',
       query: {
         category: 'SPECIAL@'
       }
-    };
-    var res = {
-      send: function (status) {
-        // then
-        assert.equal(status, 400);
-        done();
-      }
-    };
+    }
+  ], function (testcase, callback) {
+    it('should return 400 when ' + testcase.desc, function (done) {
+      // given
+      var req = {
+        query: testcase.query
+      };
 
-    // when
-    frLunchMenuController.getMenus(req, res);
+      var res = {
+        send: function (status) {
+          // then
+          assert.equal(status, 400);
+          done();
+        }
+      };
+
+      // when
+      frLunchMenuController.getMenus(req, res);
+    });
+
+    callback();
   });
 
   it('should return 404 when menus of the orderDate don\'t exist', function (done) {
@@ -392,5 +190,4 @@ describe('FrLunchMenusController#getMenus', function () {
     // when
     frLunchMenuController.getMenus(req, res);
   });
-
 });
